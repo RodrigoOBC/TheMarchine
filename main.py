aaea96840aae11e9735be5e949549cd95c708ac0
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2022-05-15 15:25:11 -0300

    craindo a extração facial

--- main.py
+++ main.py
@@ -1,38 +1,6 @@
-from os import path, listdir
-import cv2
-from PIL import Image
-from numpy import asarray, array
-from mtcnn import MTCNN
-
-
-def extrair_face(arquivo, size=(160, 160)):
-    detector = MTCNN()
-    img = Image.open(arquivo)
-    img_rgb = img.convert('RGB')
-    img_array = asarray(img_rgb)
-    results = detector.detect_faces(img_array)
-    x1, y1, w1, h1 = results[0]['box']
-    x2, y2 = x1 + w1, y1 + h1
-    face = array[x1:y2, y1:y2]
-    image = Image.fromarray(face)
-    image = image.resize(size)
-    return image
-
-
-def load_dir(diretorio_scr, diretorio_tgt):
-    for subdir in listdir(diretorio_scr):
-        file_path = diretorio_scr + subdir + '\\'
-        path_target = diretorio_tgt + subdir + '\\'
-
-        if not path.isdir(file_path):
-            continue
-        load_fotos(file_path, path_target)
-
-
-def load_fotos(diretorio_scr, diretorio_tgt):
-    print(diretorio_scr)
-    print(diretorio_tgt)
+from utils.imageData import Photo
 
 
 if __name__ == '__main__':
-    load_dir(r'data/fotos/Sr_Rodrigo/',r'data/faces/Sr_Rodrigo/')
+    Foto = Photo(r'data/fotos/', r'data/faces/')
+    Foto.load_dir()
--- utils/imageData.py
+++ utils/imageData.py
@@ -1,8 +1,25 @@
+from os import path, listdir
 import cv2
-import os
+from PIL import Image
+from numpy import asarray
+import numpy as np
+from mtcnn import MTCNN
 
 
-class WebCam:
+class Image_father:
+    def __init__(self):
+        pass
+
+    def flip_image(self, image):
+        img = image.transpose(Image.FLIP_LEFT_RIGHT)
+        return img
+
+    def rotate_90(self, image):
+        img = image.transpose(Image.ROTATE_90)
+        return img
+
+
+class WebCam(Image_father):
     def __init__(self, video=0):
         self.cap = None
         self.video = video
@@ -27,3 +44,52 @@ class WebCam:
     def save_frame(self, frame):
         cv2.imwrite(r"data/fotos_webcam/teste.jpg", frame)
         print('teste')
+
+
+class Photo(Image_father):
+    def __init__(self, diretorio_scr, diretorio_tgt):
+        super().__init__()
+        self.diretorio_scr = diretorio_scr
+        self.diretorio_tgt = diretorio_tgt
+
+    def extrair_face(self, arquivo, size=(160, 160)):
+        detector = MTCNN()
+        img = Image.open(arquivo)
+        img_RGB = img.convert('RGB')
+        img_array = asarray(img_RGB)
+        results = detector.detect_faces(img_array)
+        x1, y1, w1, h1 = results[0]['box']
+        x2, y2 = x1 + w1, y1 + h1
+        face = img_array[y1:y2, x1:x2]
+        image = Image.fromarray(face)
+        image = image.resize(size)
+        return image
+
+    def load_dir(self):
+        for subdir in listdir(self.diretorio_scr):
+            file_path = self.diretorio_scr + subdir + '/'
+            path_target = self.diretorio_tgt + subdir + '/'
+
+            if not path.isdir(file_path):
+                continue
+            self.load_fotos(file_path, path_target)
+
+    def load_fotos(self, file_path, path_target):
+        for filename in listdir(file_path):
+            src_path = file_path + filename
+            tgt_path = path_target + filename
+            flip_path = f'{path_target}flip-{filename}'
+            rotate_path = f'{path_target}rotate-{filename}'
+
+            try:
+                face = self.extrair_face(src_path)
+                flip_face = self.flip_image(face)
+                rotate_face = self.rotate_90(face)
+                face.save(tgt_path, "JPEG", quality=100, optimize=True, progressive=True)
+                print(f'save   {tgt_path}')
+                flip_face.save(flip_path, "JPEG", quality=100, optimize=True, progressive=True)
+                print(f'save   {flip_path}')
+                rotate_face.save(rotate_path, "JPEG", quality=100, optimize=True, progressive=True)
+                print(f'save   {rotate_path}')
+            except:
+                print(f'Erro na imagem {src_path}')
