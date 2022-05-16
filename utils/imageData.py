2dec0e21cf9fd17001f71ef445b7c2df0014cdb9
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2022-05-16 10:51:59 -0300

    craindo a extração facial

--- utils/imageData.py
+++ utils/imageData.py
@@ -18,11 +18,46 @@ class Image_father:
         img = image.transpose(Image.ROTATE_90)
         return img
 
+    def extrair_face(self, arquivo, size=(160, 160)):
+        detector = MTCNN()
+        img = Image.open(arquivo)
+        img_RGB = img.convert('RGB')
+        img_array = asarray(img_RGB)
+        results = detector.detect_faces(img_array)
+        x1, y1, w1, h1 = results[0]['box']
+        x2, y2 = x1 + w1, y1 + h1
+        face = img_array[y1:y2, x1:x2]
+        image = Image.fromarray(face)
+        image = image.resize(size)
+        return image
+
+    def save_fotos(self, file_path, path_target):
+        for filename in listdir(file_path):
+            src_path = file_path + filename
+            tgt_path = path_target + filename
+            flip_path = f'{path_target}flip-{filename}'
+            rotate_path = f'{path_target}rotate-{filename}'
+
+            try:
+                face = self.extrair_face(src_path)
+                flip_face = self.flip_image(face)
+                rotate_face = self.rotate_90(face)
+                face.save(tgt_path, "JPEG", quality=100, optimize=True, progressive=True)
+                print(f'save   {tgt_path}')
+                flip_face.save(flip_path, "JPEG", quality=100, optimize=True, progressive=True)
+                print(f'save   {flip_path}')
+                rotate_face.save(rotate_path, "JPEG", quality=100, optimize=True, progressive=True)
+                print(f'save   {rotate_path}')
+                print('----------------------------//--------------------------------------------')
+            except:
+                print(f'Erro na imagem {src_path}')
+
 
 class WebCam(Image_father):
     def __init__(self, video=0):
         self.cap = None
         self.video = video
+        self.FILE_SAVE_PATH = 'data/fotos_webcam/'
 
     def set_cap(self):
         self.cap = cv2.VideoCapture(self.video)
@@ -41,9 +76,13 @@ class WebCam(Image_father):
         cv2.destroyAllWindows()
         cv2.waitKey(1)
 
-    def save_frame(self, frame):
+    def save_frame(self, frame=''):
+        frame = self.capture_webcam()
         cv2.imwrite(r"data/fotos_webcam/teste.jpg", frame)
         print('teste')
+
+    def save_fotos(self, file_path='data/fotos_webcam/', path_target=''):
+        pass
 
 
 class Photo(Image_father):
@@ -52,19 +91,6 @@ class Photo(Image_father):
         self.diretorio_scr = diretorio_scr
         self.diretorio_tgt = diretorio_tgt
 
-    def extrair_face(self, arquivo, size=(160, 160)):
-        detector = MTCNN()
-        img = Image.open(arquivo)
-        img_RGB = img.convert('RGB')
-        img_array = asarray(img_RGB)
-        results = detector.detect_faces(img_array)
-        x1, y1, w1, h1 = results[0]['box']
-        x2, y2 = x1 + w1, y1 + h1
-        face = img_array[y1:y2, x1:x2]
-        image = Image.fromarray(face)
-        image = image.resize(size)
-        return image
-
     def load_dir(self):
         for subdir in listdir(self.diretorio_scr):
             file_path = self.diretorio_scr + subdir + '/'
@@ -72,24 +98,4 @@ class Photo(Image_father):
 
             if not path.isdir(file_path):
                 continue
-            self.load_fotos(file_path, path_target)
-
-    def load_fotos(self, file_path, path_target):
-        for filename in listdir(file_path):
-            src_path = file_path + filename
-            tgt_path = path_target + filename
-            flip_path = f'{path_target}flip-{filename}'
-            rotate_path = f'{path_target}rotate-{filename}'
-
-            try:
-                face = self.extrair_face(src_path)
-                flip_face = self.flip_image(face)
-                rotate_face = self.rotate_90(face)
-                face.save(tgt_path, "JPEG", quality=100, optimize=True, progressive=True)
-                print(f'save   {tgt_path}')
-                flip_face.save(flip_path, "JPEG", quality=100, optimize=True, progressive=True)
-                print(f'save   {flip_path}')
-                rotate_face.save(rotate_path, "JPEG", quality=100, optimize=True, progressive=True)
-                print(f'save   {rotate_path}')
-            except:
-                print(f'Erro na imagem {src_path}')
+            self.save_fotos(file_path, path_target)
